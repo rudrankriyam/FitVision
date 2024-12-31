4f2b55a0c433d400db8300ca4f8719a16df2f155
Rudrank Riyam <rudrankriyam@gmail.com>
2024-12-31 18:36:06 +0530

    Initial commit

---
+++ FitVision/FitVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitVisionApp.swift
+//  FitVision
+//
+//  Created by Rudrank Riyam on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FitVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
